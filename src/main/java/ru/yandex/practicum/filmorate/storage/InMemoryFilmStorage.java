1adf370da5464e353fb63bf45b8cbbf302e43cd7
asdqwe <asdaedadasczx@mail.ru>
2023-06-05 22:51:15 +0200

    TS10

---
+++ src/main/java/ru/yandex/practicum/filmorate/storage/InMemoryFilmStorage.java
@@ -0,0 +1,8 @@
+package ru.yandex.practicum.filmorate.service;
+
+import org.springframework.stereotype.Service;
+import ru.yandex.practicum.filmorate.FilmStorage;
+
+@Service
+public class InMemoryFilmStorage implements FilmStorage {
+}
